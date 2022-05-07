bf7c42fd500c46aa32c27654e277e017ef7df984
济南-前端-陶科 <taoke@qq.com>
2022-05-07 16:45:13 +0800

    🔧chore(配置文件): 修复配置问题

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,11 +5,15 @@
 declare module 'vue' {
   export interface GlobalComponents {
     AppProvider: typeof import('./components/AppProvider/AppProvider.vue')['default']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElCol: typeof import('element-plus/es')['ElCol']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElImage: typeof import('element-plus/es')['ElImage']
+    ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElRow: typeof import('element-plus/es')['ElRow']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     Header: typeof import('./components/Header/Header.vue')['default']
     HeaderNav: typeof import('./components/Header/modules/HeaderNav.vue')['default']
@@ -17,4 +21,4 @@ declare module 'vue' {
   }
 }
 
-export { }
+export {}
